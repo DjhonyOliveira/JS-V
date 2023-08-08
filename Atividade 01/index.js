63a0d3a61611656335181daa5f70b6ca954978c2
DjhonyMaga <djonatandeoliveira@hotmail.com>
2023-08-08 20:05:44 -0300

    css

--- Atividade 01/index.js
+++ Atividade 01/index.js
@@ -6,10 +6,20 @@ const newUser = new user()
 document.querySelector('.logar').addEventListener("click", function(ev) {
     ev.preventDefault()
 
-    const user = document.getElementById('user').value
-    const password = document.getElementById('password').value
+    const user = document.getElementById('user')
+    const password = document.getElementById('password')
 
-    newUser.login(user, password)
+    if (user.value == '') {
+        if (password.value == '') {
+            alert('Preencha os dados para realizar o login!')
+        } else {
+            alert('informe o usuário!')
+        }
+    } else if (password.value == '') {
+        alert('Digite sua senha!')
+    } else {
+        newUser.login(user.value, password.value)
+    }
 
     
 })
--- Atividade 01/src/cadastro.js
+++ Atividade 01/src/cadastro.js
@@ -3,15 +3,22 @@ import { user } from "/atividade 01/modules/user.js";
 document.querySelector('.enviar').addEventListener("click", (ev)=>{
     ev.preventDefault()
 
-    const name = document.getElementById('name-user').value
-    const email = document.getElementById('email-user').value
-    const password = document.getElementById('password-user').value
+    const name = document.getElementById('name-user')
+    const email = document.getElementById('email-user')
+    const password = document.getElementById('password-user')
 
-    new user(name, email, parseFloat(password))
 
-    localStorage.setItem('user', email)
-    localStorage.setItem('password', password)
-    alert('Dados gravados com sucesso!')
-
-    window.location.href = "/atividade 01/index.html"
+    if (name.value == '') {
+        alert('faltam dados a serem informados!!')
+    } else if (email.value == '') {
+        alert('faltam dados a serem informados!!')
+    } else if (password.value == '') {
+        alert('faltam dados a serem informados!!')
+    } else {
+        new user(name.value, email.value, parseFloat(password.value))
+        localStorage.setItem('user', email.value)
+        localStorage.setItem('password', password.value)
+        alert('Dados gravados com sucesso!')
+        window.location.href = "/atividade 01/index.html"
+    }
 })
